5fef79e98e6a2f1a58e47dbeaa6fc9a213ff3ad8
nunovivas <nuno.vivas@gmail.com>
2017-02-22 14:16:24 +0000

    Update Module1.vb

--- DemoConsoleApp/DemoConsoleApp/Module1.vb
+++ DemoConsoleApp/DemoConsoleApp/Module1.vb
@@ -1,7 +1,7 @@
 ﻿Module Module1
 
     Sub Main()
-        Console.WriteLine("Hello god damnit!")
+        Console.WriteLine("Hello god damnit! Changes made on the server!")
     End Sub
 
 End Module
